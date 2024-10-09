2ca267dcb6cb82597589bd5866fee5b14f2f3290
c4b1337 <jjbluezgood@gmail.com>
2024-10-09 10:26:38 +0300

    first commit

---
+++ lib/screens/startpage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class WelcomeScreens extends StatelessWidget {
+  const WelcomeScreens({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Text('Hello, World!'),
+      ),
+    );
+  }
+}
